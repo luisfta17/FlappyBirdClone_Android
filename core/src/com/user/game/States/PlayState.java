30e5558b190a36edcfdfc818be430e21046cdd44
Luis Tejero <luisfaridta@gmail.com>
2018-05-29 16:10:12 +0100

    Added tubes to playstate

--- core/src/com/user/game/States/PlayState.java
+++ core/src/com/user/game/States/PlayState.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.user.game.FlappyDemo;
 import com.user.game.sprites.Bird;
+import com.user.game.sprites.Tube;
 
 public class PlayState extends State {
 
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -38,6 +41,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
