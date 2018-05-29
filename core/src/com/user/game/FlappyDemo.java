86872ed90dba8b90ed0da910e2b7abdea406d0b3
Luis Tejero <luisfaridta@gmail.com>
2018-05-29 21:42:42 +0100

    Flappy can make flaps sounds and added background music

--- core/src/com/user/game/FlappyDemo.java
+++ core/src/com/user/game/FlappyDemo.java
@@ -2,6 +2,8 @@ package com.user.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,12 +17,16 @@ public class FlappyDemo extends ApplicationAdapter {
 
 	private GameStateManager gsm;
 	private SpriteBatch batch;
-	Texture img;
+	private Music music;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
+		music = new Gdx().audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.3f);
+		music.play();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		gsm.push(new MenuState(gsm));
 	}
@@ -32,7 +38,13 @@ public class FlappyDemo extends ApplicationAdapter {
 		gsm.update(Gdx.graphics.getDeltaTime() );
 		gsm.render(batch);
 	}
-	
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        music.dispose();
+    }
+
 //	@Override
 //	public void dispose () {
 //		batch.dispose();
--- core/src/com/user/game/sprites/Bird.java
+++ core/src/com/user/game/sprites/Bird.java
@@ -1,5 +1,7 @@
 package com.user.game.sprites;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
@@ -13,6 +15,7 @@ public class Bird {
     private Rectangle bounds;
     private Texture texture;
     private Animation birdAnimation;
+    private Sound flap;
 
     public Bird(int x, int y){
         position = new Vector3(x, y, 0);
@@ -20,6 +23,7 @@ public class Bird {
         texture = new Texture("birdanimation.png");
         birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f );
         bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
+        flap = Gdx.audio.newSound(Gdx.files.internal("sfx_wing.ogg"));
     }
 
     public void update(float dt){
@@ -47,6 +51,7 @@ public class Bird {
 
     public void jump(){
         velocity.y = 250;
+        flap.play(0.5f);
 
     }
 
@@ -56,5 +61,6 @@ public class Bird {
 
     public void dispose(){
         texture.dispose();
+        flap.dispose();
     }
 }
