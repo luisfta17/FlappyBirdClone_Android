584928a9317ace6e1c3fec9fabdf86906ce2e411
Luis Tejero <luisfaridta@gmail.com>
2018-05-29 13:04:20 +0100

    bird can not go beyond ground

--- core/src/com/user/game/sprites/Bird.java
+++ core/src/com/user/game/sprites/Bird.java
@@ -17,10 +17,14 @@ public class Bird {
     }
 
     public void update(float dt){
-        velocity.add(0, GRAVITY, 0);
+        if (position.y > 0){
+            velocity.add(0, GRAVITY, 0);
+        }
         velocity.scl(dt);
         position.add(0, velocity.y, 0);
-
+        if (position.y < 0 ){
+            position.y = 0;
+        }
         velocity.scl(1/dt);
     }
 
